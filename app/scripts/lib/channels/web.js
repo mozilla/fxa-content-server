782721f7fabcb5ad3d31dd87dc99816c2edb8023
vladikoff <vlad.filippov@gmail.com>
2014-07-08 19:04:47 -0700

    feat(views): add custom event WebChannel, add NullChannel

--- app/scripts/lib/channels/web.js
+++ app/scripts/lib/channels/web.js
@@ -4,11 +4,12 @@
 
 'use strict';
 
-// A shell of a web channel. Doesn't do anything yet, but is a useful standin.
+// A web channel to send Custom Events.
 
 define(function () {
-  function WebChannel() {
-    // nothing to do.
+  function WebChannel(name, context) {
+    this.name = name || 'WebChannelMessage';
+    this.window = context || window;
   }
 
   WebChannel.prototype = {
@@ -17,11 +18,31 @@ define(function () {
     teardown: function () {
     },
     send: function (command, data, done) {
-      if (done) {
-        done();
+      done = done || noOp;
+
+      try {
+        // Browsers can blow up dispatching the event.
+        // Ignore the blowups and return without retrying.
+        var event = this.event(command, data);
+        this.window.dispatchEvent(event);
+      } catch (e) {
+        return done(e);
       }
+      return done();
+    },
+    event: function(command, data) {
+      return new this.window.CustomEvent(this.name, {
+        detail: {
+          command: command,
+          data: data,
+          bubbles: true
+        }
+      });
     }
   };
+
+  function noOp() {
+  }
 
   return WebChannel;
 });
