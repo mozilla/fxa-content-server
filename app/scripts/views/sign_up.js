f36862f2107ea829a6f06d6146b59635358fccbd
Sai Chandramouli <schand31@asu.edu>
2016-06-28 12:42:48 -0700

    feat(password): make hints progressively useful Fixes #3731

--- app/scripts/views/sign_up.js
+++ app/scripts/views/sign_up.js
@@ -131,7 +131,6 @@ define(function (require, exports, module) {
 
     events: {
       'blur input.email': 'onEmailBlur',
-      'blur input.password': 'onPasswordBlur',
       'click #amo-migration a': 'onAmoSignIn'
     },
 
@@ -304,11 +303,6 @@ define(function (require, exports, module) {
 
       // re-throw error, it will be handled at a lower level.
       throw err;
-    },
-
-    onPasswordBlur: function () {
-      var password = this.getElementValue('.password');
-      this.checkPasswordStrength(password);
     },
 
     onEmailBlur: function () {
