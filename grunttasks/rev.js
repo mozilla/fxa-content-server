0f617b5543a9d1bcdeb2b52d9e2b53f3150c2c7c
Shane Tomlinson <stomlinson@mozilla.com>
2016-09-23 17:56:28 +0100

    fix(build): Only copy resources to dist if they are used.
    No need to copy over jquery-ui, fxa-checkbox, and styles/fonts to dist.
    
    These JS resources are included into the built bundle and not loaded
    dynamically. There is no need to copy these over.
    
    Remove jquery-ui from the rev task.
    
    issue #4177

--- grunttasks/rev.js
+++ grunttasks/rev.js
@@ -8,7 +8,6 @@ module.exports = function (grunt) {
       files: {
         src: [
           '<%= yeoman.dist %>/bower_components/**/*.js',
-          '!<%= yeoman.dist %>/bower_components/jquery-ui/**/*.js',
           '<%= yeoman.dist %>/scripts/**/*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
