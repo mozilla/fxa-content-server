f6c66d9e9bad3ad2cf9c038c4b7467be783d1a00
Vlad Filippov <vlad.filippov@gmail.com>
2016-12-28 16:30:54 -0500

    fix(tests): remove the three tabs for now

--- tests/functional/sign_up.js
+++ tests/functional/sign_up.js
@@ -384,31 +384,6 @@ define([
         // check the password was cleared
         .then(testElementValueEquals('input[type=password]', ''));
     },
-    // DISABLE THIS TEST FOR NOW , IT HAS A PROBLEM WITH WINDOW HANDLES :(
-    //
-    // 'signup, open sign-in in second tab, verify in third tab': function () {
-    //   var windowName = 'sign-up inter-tab functional test';
-    //   return this.remote
-    //     .then(fillOutSignUp(email, PASSWORD))
-    //     .then(testAtConfirmScreen(email))
-    //     .then(function () {
-    //       return FunctionalHelpers.openSignInInNewTab(this.parent, windowName);
-    //     })
-    //     .switchToWindow(windowName)
-    //
-    //     .then(testElementExists('#fxa-signin-header'))
-    //     .then(openVerificationLinkInNewTab(email, 0))
-    //
-    //     .switchToWindow('newwindow')
-    //     .then(testElementExists('#fxa-settings-header'))
-    //     .then(closeCurrentWindow(windowName))
-    //
-    //     .then(testElementExists('#fxa-settings-header'))
-    //
-    //     .then(closeCurrentWindow(''))
-    //
-    //     .then(testElementExists('#fxa-settings-header'));
-    // },
 
     'signup, open sign-up in second tab, verify in original tab': function () {
       var windowName = 'sign-up inter-tab functional test';
