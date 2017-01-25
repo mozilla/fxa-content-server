745c97e14e018d542a5b9d3201a6a371b08e49f4
Shane Tomlinson <stomlinson@mozilla.com>
2017-01-25 14:30:32 +0000

    feat(experiments): Push all knowledge of mutually exclusive tests to able.
    Knowledge of whether a test was to be run as a "mutually exclusive" test
    was spread across both able and the content server, making the content
    server code more complex than it needed to be. It would be cleaner
    to isolate that knowledge and have the front-end know nothing about
    mutual exclusivity.
    
    This commit does that.
    
    Pass in a reference to able experiments so that experiments can query
    able itself to see whether it's the active test.
    
    Goes along with https://github.com/mozilla/fxa-content-experiments/pull/53

--- app/scripts/lib/experiment.js
+++ app/scripts/lib/experiment.js
@@ -10,13 +10,11 @@ define(function (require, exports, module) {
   const MailcheckExperiment = require('lib/experiments/mailcheck');
   const Url = require('lib/url');
 
-  var CHOOSE_ABLE_EXPERIMENT = 'chooseAbExperiment';
-  var FORCE_EXPERIMENT_PARAM = 'forceExperiment';
-  var UA_OVERRIDE = 'FxATester';
+  const FORCE_EXPERIMENT_PARAM = 'forceExperiment';
+  const FORCE_EXPERIMENT_GROUP_PARAM = 'forceExperimentGroup';
+  const UA_OVERRIDE = 'FxATester';
 
-  // all available experiments that must be independent for
-  // A/B testing purposes.
-  var DEFAULT_EXPERIMENTS = {
+  const ALL_EXPERIMENTS = {
     'connectAnotherDevice': ConnectAnotherDeviceExperiment,
     'mailcheck': MailcheckExperiment
   };
@@ -33,15 +31,21 @@ define(function (require, exports, module) {
     }
 
     this.window = options.window;
-    var agent = this.window.navigator.userAgent;
-    this.forceExperiment = Url.searchParam(FORCE_EXPERIMENT_PARAM,
-        this.window.location.search);
 
+    const search = this.window.location.search;
+    this.forceExperiment = Url.searchParam(FORCE_EXPERIMENT_PARAM, search);
+    this.forceExperimentGroup = Url.searchParam(FORCE_EXPERIMENT_GROUP_PARAM, search);
+
+    const agent = this.window.navigator.userAgent;
     // if this is running in functional test mode then we do not want any unpredictable experiments
     if (agent.indexOf(UA_OVERRIDE) >= 0 && ! this.forceExperiment) {
       this.initialized = false;
       return;
     }
+
+    // reset the active experiments so that each instance
+    // of the interface creates its own copy of the list.
+    this._activeExperiments = {};
 
     this.able = options.able;
     this.account = options.account;
@@ -60,9 +64,9 @@ define(function (require, exports, module) {
     _activeExperiments: {},
 
     /**
-     * All available independent experiments
+     * All possible experiments
      */
-    _allExperiments: DEFAULT_EXPERIMENTS,
+    _allExperiments: ALL_EXPERIMENTS,
 
     /**
      * Is the user in an experiment?
@@ -95,38 +99,73 @@ define(function (require, exports, module) {
      * Makes experiment of same independent.
      */
     chooseExperiments () {
-      if (this.initialized) {
-        var choice = this.able.choose(CHOOSE_ABLE_EXPERIMENT, {
-          forceExperiment: this.forceExperiment,
-          isMetricsEnabledValue: this.metrics.isCollectionEnabled(),
-          uniqueUserId: this.user.get('uniqueUserId')
+      if (! this.initialized) {
+        return;
+      }
+
+      for (let experimentName in this._allExperiments) {
+        const isInExperiment = !! this.choose(experimentName);
+
+          // If able returns any truthy value, consider the
+          // user in the experiment.
+        if (isInExperiment) {
+          this.createExperiment(experimentName);
+        }
+      }
+    },
+
+    /**
+     * Query able for a choice.
+     *
+     * @param {String} experimentName - name of experiment to query.
+     * @returns {String|Boolean} - the result of able's choice. Returns `false`
+     *  if user is not part of experiment.
+     */
+    choose (experimentName) {
+      return this.able.choose(experimentName, {
+        // yes, this is a hack because experiments do not have a reference
+        // to able internally. This allows experiemnts to reference other
+        // experiments
+        able: this.able,
+        forceExperiment: this.forceExperiment,
+        forceExperimentGroup: this.forceExperimentGroup,
+        isMetricsEnabledValue: this.metrics.isCollectionEnabled(),
+        uniqueUserId: this.user.get('uniqueUserId')
+      });
+    },
+
+    /**
+     * Create an experiment and add it to the list of active experiments.
+     *
+     * @param {String} experimentName - name of experiment to create.
+     */
+    createExperiment (experimentName) {
+      const ExperimentConstructor = this._allExperiments[experimentName];
+      if (_.isFunction(ExperimentConstructor)) {
+        const experiment = new ExperimentConstructor();
+        const initResult = experiment.initialize(experimentName , {
+          able: this.able,
+          account: this.account,
+          metrics: this.metrics,
+          notifier: this.notifier,
+          translator: this.translator,
+          user: this.user,
+          window: this.window
         });
 
-        var ExperimentConstructor = this._allExperiments[choice];
-        if (ExperimentConstructor) {
-          var experiment = new ExperimentConstructor();
-          var initResult = experiment.initialize(choice , {
-            able: this.able,
-            account: this.account,
-            metrics: this.metrics,
-            notifier: this.notifier,
-            translator: this.translator,
-            user: this.user,
-            window: this.window
-          });
-
-          /**
-           * 'initResult' may be false if the view does not have the require components, such as 'user', or 'account'.
-           * Those components are required to make a correct decision about the experiment.
-           * 'initialize' does not throw because some experiments only work on particular views.
-           *
-           * If experiment failed to initialized then do not add it to active experiments.
-           * Also if the experiment is not active then the user gets the default view options with no modifications.
-           */
-          if (initResult) {
-            this._activeExperiments[choice] = experiment;
-            this.metrics.logExperiment(choice, experiment._groupType);
-          }
+        /**
+         * 'initResult' may be false if the view does not have the
+         * required components, such as 'user', or 'account'.
+         * Those components are required to make a correct decision
+         * about the experiment. 'initialize' does not throw
+         * because some experiments only work on particular views.
+         *
+         * If experiment failed to initialized then do not add it
+         * to active experiments. Also if the experiment is not active then the user gets the default view options with no modifications.
+         */
+        if (initResult) {
+          this._activeExperiments[experimentName] = experiment;
+          this.metrics.logExperiment(experimentName, experiment._groupType);
         }
       }
     }
