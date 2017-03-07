cfb75f1f81c4dd1df8909ecb556506ef902e2897
Vijay Budhram <vbudhram@mozilla.com>
2017-03-06 23:32:15 -0500

    feat(emails): Initial UX for additional emails

--- app/scripts/models/account.js
+++ app/scripts/models/account.js
@@ -1088,6 +1088,31 @@ define(function (require, exports, module) {
         this.get('sessionToken'),
         email
       );
+    },
+
+    /**
+     * Get emails associated with user.
+     *
+     * @returns {Promise}
+     */
+    getEmails () {
+      return this._fxaClient.getEmails(
+        this.get('sessionToken')
+      );
+    },
+
+    /**
+     * Associates a new email to a users account.
+     *
+     * @param {String} email
+     *
+     * @returns {Promise}
+     */
+    createEmail (email) {
+      return this._fxaClient.createEmail(
+        this.get('sessionToken'),
+        email
+      );
     }
   }, {
     ALLOWED_KEYS: ALLOWED_KEYS,
