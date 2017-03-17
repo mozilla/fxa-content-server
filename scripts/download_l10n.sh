14204297f8b1c3c97bb98ac65c95bdd172a25b77
Edouard Oger <eoger@fastmail.com>
2017-03-17 17:50:05 -0400

    fix(setup): use github relative path for l10n setup
    Fixes #4819

--- scripts/download_l10n.sh
+++ scripts/download_l10n.sh
@@ -6,7 +6,7 @@ if [ -z "$FXA_L10N_SHA" ]; then
     FXA_L10N_SHA="master"
 fi
 
-DOWNLOAD_PATH="https://github.com/mozilla/fxa-content-server-l10n.git#$FXA_L10N_SHA"
+DOWNLOAD_PATH="mozilla/fxa-content-server-l10n#$FXA_L10N_SHA"
 
 echo "Downloading L10N files from $DOWNLOAD_PATH..."
 # Download L10N using npm
