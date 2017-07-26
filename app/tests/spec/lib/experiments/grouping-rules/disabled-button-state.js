29833d8a346a4ad76b4db448605faebe50f07c5c
Vlad Filippov <vlad.filippov@gmail.com>
2017-07-26 11:18:59 -0400

    add new test files

---
+++ app/tests/spec/lib/experiments/grouping-rules/disabled-button-state.js
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+define(function (require, exports, module) {
+  'use strict';
+
+  const { assert } = require('chai');
+  const Account = require('models/account');
+  const Experiment = require('lib/experiments/grouping-rules/send-sms-install-link');
+  const sinon = require('sinon');
+
+  describe('lib/experiments/grouping-rules/disabled-button-state', () => {
+    let account;
+    let experiment;
+
+    before(() => {
+      account = new Account();
+      experiment = new Experiment();
+    });
+
+    describe('choose', () => {
+
+    });
+  });
+});
