5b8faac7a6cc53c8f35edfbfedc7d22e3cc91003
vladikoff <vlad.filippov@gmail.com>
2017-08-14 15:13:04 -0400

    fix(keys): clean up

--- app/scripts/lib/crypto/relier-keys.js
+++ app/scripts/lib/crypto/relier-keys.js
@@ -8,10 +8,7 @@
 
 'use strict';
 
-define([
-  'p-promise',
-  'lib/constants'
-], function (p, Constants) {
+define([], function () {
 
   const scopedKeys = new window.fxaCryptoDeriver.ScopedKeys();
 
@@ -35,12 +32,6 @@ define([
     if (! keys.kB) {
       throw new Error('Cant derive relier keys: missing kB');
     }
-    if (! uid) {
-      throw new Error('Cant derive relier keys: missing uid');
-    }
-    if (! clientId) {
-      throw new Error('Cant derive relier keys: missing rid');
-    }
 
     // scopedKeyTimestamp -> comes from the assertion, in the fxageneration....
     // after the login...
@@ -55,53 +46,7 @@ define([
     });
   }
 
-  /**
-   * Given master key material, the name of the key class in question,
-   * the user id and the relier id, generate the appropriate derived key
-   * as a JWK object.
-   *
-   * The key is produced by HKDF-deriving 64 bytes with relier-specific
-   * HKDF context info.  The first 32 bytes are used as an opaque key id
-   * and the second 32 bytes are the actual key material.
-   *
-   * Input options:
-   *    inputKey:     hex string giving the master key material.
-   *    keyClassTag:  string identifying the type of key to derive; this
-   *                  forms part of the HKDF context info and is included
-   *                  in the key id.
-   *    uid:          string identifying the user who owns the keys.
-   *    clientId:     string identifying the relier for whom the key is
-   *                  being derived; this forms part of the HKDF context
-   *                  info to ensure unique keys for each relier.
-   *
-   * Output:
-   *    A promise that will resolve with a JWK object representing the
-   *    derived key.
-   */
-  function generateDerivedKey(options) {
-    var key = sjcl.codec.hex.toBits(options.inputKey);
-    var salt = sjcl.codec.hex.toBits('');
-    var contextInfo = sjcl.codec.utf8String.toBits(
-      Constants.RELIER_KEYS_CONTEXT_INFO_PREFIX +
-      options.keyClassTag + ':' + options.clientId
-    );
-    return hkdf(key, contextInfo, salt, Constants.RELIER_KEYS_LENGTH * 2)
-      .then(function (out) {
-        var keySizeBits = Constants.RELIER_KEYS_LENGTH * 8;
-        var keyId = sjcl.bitArray.bitSlice(out, 0, keySizeBits);
-        var keyBytes = sjcl.bitArray.bitSlice(out, keySizeBits, keySizeBits * 2);
-        return {
-          kid: options.keyClassTag + '-' + base64url.encode(keyId),
-          k: base64url.encode(keyBytes),
-          kty: 'oct',
-          rid: options.clientId,
-          uid: options.uid
-        };
-      });
-  }
-
   return {
-    deriveRelierKeys: deriveRelierKeys,
-    generateDerivedKey: generateDerivedKey
+    deriveRelierKeys: deriveRelierKeys
   };
 });
