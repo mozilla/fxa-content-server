2cbd5cb70b0b29943d09c8302e4d9d3d4e441e0d
vershwal <vershwal.princi@gmail.com>
2017-08-18 02:02:20 +0530

    fix(test) : Fixed some tests.

--- app/tests/spec/models/account.js
+++ app/tests/spec/models/account.js
@@ -478,10 +478,10 @@ define(function (require, exports, module) {
         describe('unverified', function () {
           beforeEach(function () {
             account.set('sessionToken', SESSION_TOKEN);
+            account.set('verificationMethod', 'email');
 
             sinon.stub(fxaClient, 'recoveryEmailStatus', function () {
               return p({
-                verificationMethod: VerificationMethods.EMAIL,
                 verificationReason: VerificationReasons.SIGN_IN,
                 verified: false
               });
