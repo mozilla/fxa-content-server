f5c2878971c52c10ea31c808ed59d816b2848f69
Phil Booth <pmbooth@gmail.com>
2017-12-16 12:57:43 -0600

    chore(deps): update celebrate

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 'use strict';
-const celebrate = require('celebrate');
+const { celebrate } = require('celebrate');
 const cors = require('cors');
 const logger = require('./logging/log')('server.routes');
 
