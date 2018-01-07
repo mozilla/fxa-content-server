065ca9a0771967c943613e7acac33f1a7a74e178
mutdmour <mutdmour@gmail.com>
2018-01-07 16:28:10 +0200

    fix(testing): #4982 fix esc keyup event

--- tests/functional/lib/helpers.js
+++ tests/functional/lib/helpers.js
@@ -1515,7 +1515,7 @@ define([
           });
           target.dispatchEvent(event);
         }, [ selector ]);
-  }
+  });
 
   const clearBrowserNotifications = thenify(function () {
     return this.parent
